a433defaa6eeebc47fdf476c07c89ace2eef065b
FedorSaga <fedor.sagatelian@icloud.com>
2021-02-12 16:39:13 +0100

    first commit

--- main.py
+++ main.py
@@ -1,29 +1,12 @@
 from tkinter import*
 from random import*
-
-def open_shop():
-    global boost, points
-    
-    shop = Tk()
-    shop.geometry('700x600')
-    shop.title('Ultimate clicker shop')
-    shop.config(bg='white')
-    
-    double_lfrm = LabelFrame(shop, text='2x boost')
-    double_lfrm.place(relx=0.4, relwidth=0.2)
-    
-    double_lbl = Label(double_lfrm, text='cost: 100P', bg='black')
-    double_lfrm.pack()
     
 def click():
-    global clicks, colors, points, boost
+    global clicks, colors
     rand_color = choice(colors)
     main_btn.config(bg=rand_color, activebackground=rand_color)
     clicks += 1
-    points += boost * 1
-    allclicks_lbl.config(text='You clicked '+str(clicks)+' times.')
-    point_lbl.config(text='You have '+str(clicks)+' points.')
-    
+    allclicks_lbl.config(text='You clicked '+str(clicks)+' times.')    
 
 root = Tk()
 root.geometry('400x400')
@@ -38,13 +21,5 @@ main_btn.place(relx=0.27, rely=0.387, relheight=0.4, relwidth=0.452)
 
 allclicks_lbl = Label(text='You clicked '+str(clicks)+' times.', bg='white')
 allclicks_lbl.place(relx=0.29, rely=0.15, relheight=0.05, relwidth=0.4)
-
-points = 0
-boost = 1
-point_lbl = Label(text='You have '+str(points)+' points.', bg='white')
-point_lbl.place(relx=0.29, rely=0.25, relwidth=0.4, relheight=0.05)
-
-shop_btn = Button(text='shop', command=open_shop)
-shop_btn.place(relx=0.001, rely=0.001, relwidth=0.2, relheight=0.09)
 
 root.mainloop()
